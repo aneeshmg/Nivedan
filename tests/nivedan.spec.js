dc1a512ea9a4b10d72a713bc9e4957c6067797df
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-11-13 14:36:29 +0530

    fix test case

--- tests/nivedan.spec.js
+++ tests/nivedan.spec.js
@@ -14,23 +14,24 @@ const url = "http://localhost:8000"
 
 describe("test get requests", () => {
 
-    it("should respond with callback successfully", done => {
+    it.only("should respond with callback successfully", done => {
+        
         nock(url).get("/_status").reply(SUCCESS_CODE, function () {
             return sampleJSONResponse
         })
-
-        let callbackSpy = sinon.spy()
-
-        nivedan.get(url, {}, {}, callbackSpy)
-
-        // TODO: fix this timeout, callback call not getting detected without this timeout...
-        setTimeout(() => {
+        let callback = (err, data) => {
+            expect(callbackSpy.called).to.be.true
             expect(callbackSpy.calledOnce).to.be.true
             expect(callbackSpy.calledWith(null, sampleJSONResponse)).to.be.true
+            expect(data).to.not.be.null
+            expect(err).to.be.null
+            expect(data).to.be.equal(sampleJSONResponse)
             done()
-            callbackSpy.restore()
-        }, 10)
-        
+        }
+
+        let callbackSpy = sinon.spy(callback)
+
+        nivedan.get(url, {}, {}, callbackSpy)      
 
     })
 
@@ -39,8 +40,7 @@ describe("test get requests", () => {
         nock(url).get("/_status").reply(SUCCESS_CODE, function () {
             return sampleJSONResponse
         })
-        
-    
+          
         nivedan.get(url, {}, {}).then(response => {
             expect(response).to.not.be.null
             expect(response).to.be.equal(sampleJSONResponse)
